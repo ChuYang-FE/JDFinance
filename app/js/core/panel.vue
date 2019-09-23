7254d156604c02e03d498a7171910bdc8dd9ee00
Young <865428527@qq.com>
2019-09-23 17:02:34 +0800

    抽象btn、panel、slider

---
+++ app/js/core/panel.vue
@@ -0,0 +1,35 @@
+<template>
+  <section :class="[panelClass, cname]">
+    <h4>-{{ title }}-</h4>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  //接收自定义样式cname和标题title
+  props: {
+    cname: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  //默认样式panelClass为默认的panel
+  data() {
+    return {
+      panelClass: "panel",
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import "../../css/element.scss";
+.panel {
+  @include panel;
+}
+</style>
---
+++ app/js/core/slider.vue
@@ -0,0 +1,47 @@
+<template>
+  <section :class="cname"> <!--自定义样式cname，因为swiper.css已经写好了默认样式，所以不必写。-->
+    <swiper :options="options" :not-next-tick="options.notNextTick"> <!--官方要求使用options来传参-->
+      <swiper-slide v-for="item in items" :key="item.href">
+        <router-link :to="{ name: item.href}">
+          <img :src="item.src" alt="">
+        </router-link>
+      </swiper-slide>
+      <div class="swiper-pagination" v-if="options.pagination"/>
+    </swiper>
+  </section>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper"
+export default {
+  components: {
+    swiper,
+    swiperSlider,
+  },
+  props: {
+    options: {
+      type: Object,
+      default () { //es6写法
+        return {
+          autoplay: true,//自动轮播
+          loop: true,//循环
+          pagination: { //指示器
+            el: ".swiper-pagination",
+          },
+          notNextTick: false,
+        }
+      }
+    },
+    items: {
+      type: Object,
+      default () {
+        return []
+      },
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  @import "~swiper/dist/css/swiper.css"
+</style>
