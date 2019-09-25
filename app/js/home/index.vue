84b77978f9db4fa6b8ff7f7b7ce205120754f3e7
Young <865428527@qq.com>
2019-09-25 16:08:31 +0800

    极速借贷、理财精选

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -3,6 +3,8 @@
         <Heador></Heador>
         <Slider></Slider>
         <Novice></Novice>
+        <Borrow></Borrow>
+        <Money></Money>
     </div>
 </template>
 
@@ -10,11 +12,15 @@
 import Heador from "../public/header"  //header组件和默认的HTML的header标签一致，所以要换名。
 import Slider from "./hslider"
 import Novice from "./novice"
+import Borrow from "./borrow"
+import Money from "./money"
 export default {
   components: {
     Heador,
     Slider,
     Novice,
+    Borrow,
+    Money,
   },
 }
 </script>
