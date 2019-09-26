f8a73fecac1a0918320c6319d92e8a8afcad6f07
Young <865428527@qq.com>
2019-09-26 15:51:39 +0800

    首页完成

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -5,15 +5,24 @@
         <Novice></Novice>
         <Borrow></Borrow>
         <Money></Money>
+        <Product></Product>
+        <Life></Life>
+        <Feeter></Feeter>
+        <Navbar></Navbar>
     </div>
 </template>
 
 <script>
-import Heador from "../public/header"  //header组件和默认的HTML的header标签一致，所以要换名。
-import Slider from "./hslider"
-import Novice from "./novice"
-import Borrow from "./borrow"
-import Money from "./money"
+import Heador from "../public/header"; //header组件和默认的HTML的header标签一致，所以要换名。
+import Feeter from "../public/footer"; //y
+import Navbar from "../public/navbar";
+import Slider from "./hslider";
+import Novice from "./novice";
+import Borrow from "./borrow";
+import Money from "./money";
+import Product from "./product";
+import Life from "./life";
+
 export default {
   components: {
     Heador,
@@ -21,8 +30,12 @@ export default {
     Novice,
     Borrow,
     Money,
-  },
-}
+    Product,
+    Life,
+    Feeter,
+    Navbar,
+  }
+};
 </script>
 
 
