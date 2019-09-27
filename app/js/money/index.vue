195ad4af612454fba6f273b3078c8a7e58881587
Young <865428527@qq.com>
2019-09-27 16:54:11 +0800

    理财money页面完成

---
+++ app/js/money/index.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+    <div>
+        <Heador></Heador>
+        <Slider></Slider>
+        <Ada></Ada>
+        <Money></Money>
+        <Adb></Adb>
+        <Finance></Finance>
+        <Adc ></Adc>
+        <Feeter cname="mfooter"></Feeter>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header"; //header组件和默认的HTML的header标签一致，所以要换名。
+import Feeter from "../public/footer"; //y
+import Navbar from "../public/navbar";
+import Slider from "./mslider";
+import Ada from "./ada";
+import Money from "./money";
+import Adb from "./adb";
+import Finance from "./finance";
+import Adc from "./adc";
+
+export default {
+  components: {
+    Heador,
+    Slider,
+    Feeter,
+    Navbar,
+    Ada,
+    Money,
+    Adb,
+    Finance,
+    Adc,
+  }
+};
+</script>
+
+<style lang="scss">
+  .mfooter{
+    margin-top: 0 !important;
+    ul{
+      li:first-child{
+        display: none;
+      }
+    }
+  }
+</style>
+
+
--- app/js/router/index.js
+++ app/js/router/index.js
@@ -1,16 +1,20 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Home from "../home/index.vue"
+import Money from "../money/index.vue"
 import "../../css/reset.scss"
 
 Vue.use(Router)
 
 export default new Router({
-    routes: [
-        {
-            path: "/",
-            name: "Home",
-            component: Home,
-        },
-    ],
+  routes: [{
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/money",
+    name: "money",
+    component: Money,
+  }],
 })
